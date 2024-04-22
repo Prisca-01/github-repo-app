1fd93fc6ac7764e4caed1f2e04f7f4802ad70fb2
Prisca-01 <priscaonyemaechi26@gmail.com>
2024-04-22 11:05:09 +0100

    done with the Ui setup

---
+++ my-project/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline text-red-400 text-8xl  text-center w-full">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
